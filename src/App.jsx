f329cd198abf18615bd48d59e1a4badd370d93c8
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2024-01-15 17:54:03 -0600

    agregando fotos en la galería

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import './App.css'
+import { useEffect, useState } from 'react';
+import image from './fotos/1.jpg';
+import image2 from './fotos/2.jpg';
+import image3 from './fotos/3.jpg';
+import image4 from './fotos/4.jpg';
+import image5 from './fotos/5.jpg';
+import image6 from './fotos/6.jpg';
 
 function App() {
+
+  const [imageList, setImageList] = useState([]);
+
+  useEffect(() => {
+    setImageList([image, image2, image3, image4, image5, image6]);
+    
+  }, []);
+
+  console.log(imageList);
 
   return (
     <>
@@ -13,6 +29,12 @@ function App() {
         <p>
           Por: Javier Sebastián Valle Balsells
         </p>
+      </div>
+
+      <div className="image-gallery">
+        {imageList.map((image, index) => (
+          <img key={index} src={image} alt={`Image ${index}`} />
+        ))}
       </div>
     </>
   )
