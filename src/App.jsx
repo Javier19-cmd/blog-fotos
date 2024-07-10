d463d1209b12e6c629039af9371ac854d418db24
Javier Sebastián Valle <63107636+Javier19-cmd@users.noreply.github.com>
2024-07-10 17:44:35 -0600

    adding some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,16 @@ import image4 from './fotos/4.jpg';
 import image5 from './fotos/5.jpg';
 import image6 from './fotos/6.jpg';
 import image7 from './fotos/7.jpg';
-import image8 from './fotos/8.jpg';
-import image9 from './fotos/9.jpg';
-import image10 from './fotos/10.jpg';
-import image11 from './fotos/11.jpg';
-import image12 from './fotos/12.jpg';
-import image13 from './fotos/13.jpg';
-import image14 from './fotos/14.jpg';
-import image15 from './fotos/15.jpg';
-import image16 from './fotos/16.jpg';
-import image17 from './fotos/17.jpg';
-import image18 from './fotos/18.jpg';
+import image8 from './fotos/9.jpg';
+import image9 from './fotos/10.jpg';
+import image10 from './fotos/11.jpg';
+import image11 from './fotos/12.jpg';
+import image12 from './fotos/13.jpg';
+import image13 from './fotos/14.jpg';
+import image14 from './fotos/15.jpg';
+import image15 from './fotos/16.jpg';
+import image16 from './fotos/17.jpg';
+import image17 from './fotos/18.jpg';
 
 function App() {
   const [imageList, setImageList] = useState([]);
@@ -25,7 +24,7 @@ function App() {
   const [selectedImage, setSelectedImage] = useState('');
 
   useEffect(() => {
-    setImageList([image, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17, image18]);
+    setImageList([image, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17]);
   }, []);
 
   const handleImageClick = (imageUrl) => {
